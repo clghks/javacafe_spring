4cd8fbc7aee214720acd14f2055358518ca1a138
chihwan <chihwan@CihwanChoiui-MacBook-Pro-2.local>
2016-07-01 17:43:06 +0900

    초난감 Dao 만들기

---
+++ UserDAO/src/com/javacafe/user/AppMain.java
@@ -0,0 +1,24 @@
+package com.javacafe.user;
+
+import java.sql.SQLException;
+
+import com.javacafe.user.dao.UserDao;
+import com.javacafe.user.domain.User;
+
+public class AppMain {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		UserDao userDao = new UserDao();
+		
+		User user = new User();
+		user.setId("clghks");
+		user.setName("최치환");
+		user.setPassword("married");
+		
+		userDao.add(user);
+		
+		System.out.println(user.getId() + " 등록 성공");
+		
+		User user2 = userDao.get(user.getId());
+		System.out.println(user2.getName());
+	}
+}
---
+++ UserDAO/src/com/javacafe/user/dao/UserDao.java
@@ -0,0 +1,50 @@
+package com.javacafe.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.javacafe.user.domain.User;
+
+public class UserDao {
+	
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
+		
+		PreparedStatement preparedStatement = connection.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
+		preparedStatement.setString(1, user.getId());
+		preparedStatement.setString(2, user.getName());
+		preparedStatement.setString(3, user.getPassword());
+		
+		preparedStatement.executeUpdate();
+
+		preparedStatement.close();
+		connection.close();
+	}
+	
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
+		
+		PreparedStatement preparedStatement = connection.prepareStatement("select * from users where id = ?");
+		preparedStatement.setString(1, id);
+		
+		ResultSet resultSet = preparedStatement.executeQuery();
+		resultSet.next();
+		
+		User user = new User();
+		user.setId(resultSet.getString("id"));
+		user.setName(resultSet.getString("name"));
+		user.setPassword(resultSet.getString("password"));
+		
+		preparedStatement.close();
+		connection.close();
+		resultSet.close();
+		
+		return user;
+	}
+
+}
