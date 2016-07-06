d0f1ac09d90519d2fb1b9b028436a04c374b4e7b
chihwan <clghks@gmail.com>
2016-07-06 23:58:42 +0900

    오브젝트 팩토리의 활용 (2/2)

--- UserDAO/src/com/javacafe/user/dao/DaoFactory.java
+++ UserDAO/src/com/javacafe/user/dao/DaoFactory.java
@@ -2,23 +2,21 @@ package com.javacafe.user.dao;
 
 public class DaoFactory {
 	public UserDao userDao(){
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDao userDao = new UserDao(connectionMaker);
-		
+		UserDao userDao = new UserDao(connectionMaker());		
 		return userDao;
 	}
 	
 	public AccountDao accountDao(){
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		AccountDao accountDao = new AccountDao(connectionMaker);
-		
+		AccountDao accountDao = new AccountDao(connectionMaker());		
 		return accountDao;
 	}
 	
 	public MessageDao messageDao(){
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		MessageDao messageDao = new MessageDao(connectionMaker);
-		
+		MessageDao messageDao = new MessageDao(connectionMaker());		
 		return messageDao;
+	}
+	
+	public ConnectionMaker connectionMaker(){
+		return new DConnectionMaker();
 	}
 }
