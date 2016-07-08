52ede6740708a2f708fd414a0a42e127817b926c
chihwan <chihwan@CihwanChoiui-MacBook-Pro-2.local>
2016-07-08 14:00:10 +0900

    DaoFactory를 직접 사용할 때

--- UserDAO/src/com/javacafe/user/SingletonMain.java
+++ UserDAO/src/com/javacafe/user/SingletonMain.java
@@ -1,18 +1,18 @@
 package com.javacafe.user;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 import com.javacafe.user.dao.DaoFactory;
 import com.javacafe.user.dao.UserDao;
 
 public class SingletonMain {
 	public static void main(String[] args) {
-		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-		UserDao userDao = context.getBean("userDao", UserDao.class);
-		UserDao userDao1 = context.getBean("userDao", UserDao.class);
+		DaoFactory daoFactory = new DaoFactory();
+		
+		UserDao userDao = daoFactory.userDao();
+		UserDao userDao1 = daoFactory.userDao();
 
 		System.out.println(userDao);
 		System.out.println(userDao1);
 	}
 }
+
+
